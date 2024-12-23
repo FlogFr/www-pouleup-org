eb1510c5b69bf270bdce63a40f27fcb9a05a9812
Florian Grignon <grignon.florian@gmail.com>
2024-12-23 12:24:44 +0100

    New first publish

--- publishconf.py
+++ publishconf.py
@@ -8,7 +8,8 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = "https://pouleup.org"
+# SITEURL = "https://pouleup.org"
+SITEURL = "https://flogfr.github.io/www-pouleup-org"
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = "feeds/all.atom.xml"
